eea94642fa3ae3c66a2e3567ce0fd8fc7d5d8bc8
iammahir2020 <1810659@iub.edu.bd>
2022-05-11 15:59:48 +0600

    doctors portal server coding start

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Doctors Portal Server is LIVE!");
+});
+
+app.listen(port, () => {
+  console.log("Listening to port", port);
+});
